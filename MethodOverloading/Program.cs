f5f2dcb95668b86d54597202f68304d25acf0d8a
TonyDolenti <antoniobdolenti@gmail.com>
2023-01-14 10:59:42 -0500

    initial commit

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,7 @@
-﻿namespace MethodOverloading
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿
+
+using MethodOverloading;
+
+var answer = Methods.Add(11, 1, true);
+Console.WriteLine(answer);
+
